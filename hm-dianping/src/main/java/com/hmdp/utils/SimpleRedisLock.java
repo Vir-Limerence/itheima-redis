df7fa4e429707a9d7085a565f5de5e68605fb8c4
maqi <2466570430@qq.com>
2024-05-18 20:38:13 +0800

    通过Redis实现分布式锁（版本2，防止锁🔒被误删）

--- hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -10,7 +10,7 @@ public class SimpleRedisLock implements ILock{
     private StringRedisTemplate stringRedisTemplate;
     private String name;
     private static final String KEY_PREFIX = "lock:";
-
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 //    private static final String
 
     public SimpleRedisLock(StringRedisTemplate stringRedisTemplate, String name) {
@@ -23,12 +23,20 @@ public class SimpleRedisLock implements ILock{
         long threadId = Thread.currentThread().getId();
         //获取锁
         Boolean success = stringRedisTemplate.opsForValue()
-                .setIfAbsent(KEY_PREFIX+name, threadId+"", timeoutSec, TimeUnit.SECONDS);
+                .setIfAbsent(KEY_PREFIX+name, ID_PREFIX+threadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(KEY_PREFIX+name);
+        //1.获取当前锁中的线程标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        //2.获取Redis中锁的标识
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        //3.判断是否一致，一致则可以释放锁
+        if(threadId.equals(id)){
+            stringRedisTemplate.delete(KEY_PREFIX+name);
+        }
+
     }
 }
